3260d3979ba4316c40432fb877f8e8b4c60ea148
Dhruv786Sharma <115816524+Dhruv786Sharma@users.noreply.github.com>
2023-09-07 13:44:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function new_image()
         block_image_object = Img
 
         block_image_object.scaleToWidth(700);
-        block_image_object.scaleToHeight(610);
+        block_image_object.scaleToHeight(510);
         block_image_object.set({
             top:0,
             left:0
